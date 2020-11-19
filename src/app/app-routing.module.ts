e8e8d9c1c50cefe22941959bf013c18502dc4d06
Hassan Khousheish <khousheish@live.com>
2020-11-19 08:07:16 +0100

    Add Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { RoutePathMatchOptions, ViewRoutes } from '@Consts/routes.enum';
+import { Page404Component } from '@Pages/page404/page404.component';
+import { Module } from '@Types/module.types';
+
+const routes: Routes = [
+  {
+    path: ViewRoutes.Posts,
+    loadChildren: (): Promise<Module> => import('./modules/posts/posts.module')
+      .then((module: Module): Module => module.PostsModule),
+  },
+  {
+    path: ViewRoutes.NotFound,
+    component: Page404Component,
+  },
+  {
+    path: ViewRoutes.Empty,
+    redirectTo: ViewRoutes.Posts,
+    pathMatch: RoutePathMatchOptions.Full,
+  },
+  {
+    path: ViewRoutes.Others,
+    redirectTo: `/${ViewRoutes.NotFound}`,
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
